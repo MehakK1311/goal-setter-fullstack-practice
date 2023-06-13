b99416d5f2cbec916346c5b5decbe81befc48208
MehakK1311 <mehakhanna1311@gmail.com>
2023-06-13 23:00:44 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const goalRoutes = require("./routes/goalRoutes");
+
+
 
 const app = express();
+const router = express.Router();
 
-app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
+// app.use(
+//     cors({
+//       credentials: true,
+//       origin: "http://localhost:3000",
+//     })
+//   );
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.use(express.json());
+
+app.use('/api/goals', goalRoutes)
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
